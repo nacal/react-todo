2980a508527ca37f14721ec5139c97789c4788ab
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-11 16:10:07 +0900

    Develop Form styles

--- src/Form.tsx
+++ src/Form.tsx
@@ -42,20 +42,44 @@ const Form = (props: FormProps) => {
             ゴミ箱を空にする
           </button>
         ) : (
-          <form onSubmit={(e) => handleOnSubmit(e)}>
-            <input
-              type="text"
-              value={text}
-              disabled={props.filter === 'checked'}
-              onChange={(e) => setText(e.target.value)}
-            />
-            <input
-              type="submit"
-              value='追加'
-              disabled={props.filter === 'checked'}
-              onChange={(e) => handleOnSubmit(e)}
-            />
-          </form>
+          <form
+            onSubmit={(e) => handleOnSubmit(e)}
+            className="flex items-center max-w-md mx-auto bg-white rounded-full"
+          >
+            <div className="w-full">
+              <input
+                type="search"
+                value={text}
+                disabled={props.filter === 'checked'}
+                onChange={(e) => setText(e.target.value)}
+                className="w-full px-4 py-1 text-gray-900 rounded-full focus:outline-none"
+                placeholder="add tasks"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                disabled={props.filter === 'checked'}
+                onChange={(e: any) => handleOnSubmit(e)}
+                className="flex items-center justify-center w-12 h-12 bg-gray-900 text-gray-100 rounded-full"
+              >
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    fill="#fff"
+                    d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </form >
         )
       }
     </>
