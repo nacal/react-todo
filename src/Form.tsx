59a7d9112966d9af7867ac8fc87ecaf85ea890b9
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-11 16:33:00 +0900

    Develop form styles

--- src/Form.tsx
+++ src/Form.tsx
@@ -31,57 +31,60 @@ const Form = (props: FormProps) => {
     props.setTodos(newTodos);
   }
 
+  const disable: boolean = (props.filter === 'checked' || props.filter === 'removed');
+
   return (
     <>
-      { props.filter === 'removed' ?
-        (
+      {(() => {
+        if (props.filter === 'removed') {
+          return (
+            <button
+              onClick={() => handleOnEmpty()}
+              disabled={props.todos.filter((todo) => todo.removed).length === 0}
+            >
+              ゴミ箱を空にする
+            </button>
+          )
+        }
+      })()}
+      <form
+        onSubmit={(e) => handleOnSubmit(e)}
+        className="flex items-center max-w-md mx-auto bg-white rounded-full"
+      >
+        <div className="w-full">
+          <input
+            type="search"
+            value={text}
+            disabled={disable}
+            onChange={(e) => setText(e.target.value)}
+            className="w-full px-4 py-1 text-gray-900 rounded-full focus:outline-none disabled:opacity-50"
+            placeholder="add tasks"
+          />
+        </div>
+        <div>
           <button
-            onClick={() => handleOnEmpty()}
-            disabled={props.todos.filter((todo) => todo.removed).length === 0}
+            type="submit"
+            disabled={disable}
+            onChange={(e: any) => handleOnSubmit(e)}
+            className="flex items-center justify-center w-12 h-12 bg-gray-900 text-gray-100 rounded-full disabled:opacity-50"
           >
-            ゴミ箱を空にする
-          </button>
-        ) : (
-          <form
-            onSubmit={(e) => handleOnSubmit(e)}
-            className="flex items-center max-w-md mx-auto bg-white rounded-full"
-          >
-            <div className="w-full">
-              <input
-                type="search"
-                value={text}
-                disabled={props.filter === 'checked'}
-                onChange={(e) => setText(e.target.value)}
-                className="w-full px-4 py-1 text-gray-900 rounded-full focus:outline-none"
-                placeholder="add tasks"
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                fill="#fff"
+                d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"
               />
-            </div>
-            <div>
-              <button
-                type="submit"
-                disabled={props.filter === 'checked'}
-                onChange={(e: any) => handleOnSubmit(e)}
-                className="flex items-center justify-center w-12 h-12 bg-gray-900 text-gray-100 rounded-full"
-              >
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    fill="#fff"
-                    d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </form >
-        )
-      }
+            </svg>
+          </button>
+        </div>
+      </form >
     </>
   )
 }
